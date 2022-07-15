2dea203c5d5ffea014ccbdf64032bd8ffb6a8afa
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-07-15 13:01:16 +0300

    Design header using hero-icons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text, Image, TextInput, ScrollView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {AdjustmentsIcon, ChevronDownIcon, SearchIcon, UserIcon} from 'react-native-heroicons/outline'
+import Categories from "../components/Categories";
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text className='text-red-900'>HomeScreen</Text>
-      <Text className='text-red-900'>HomeScreen</Text>
-      <Text className='text-red-900'>HomeScreen</Text>
-      <Text className='text-red-900'>HomeScreen</Text>
-      <Text className='text-red-900'>HomeScreen</Text>
-      <Text className='text-red-900'>HomeScreen</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default HomeScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className='bg-white pt-5'>
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <ChevronDownIcon size={20} color={"#00CCBB"}/>
+          </Text>
+          
+        </View>
+        <UserIcon size={35} color={"#00CCBB"} />
+
+      </View>
+
+      {/* Search */}
+      <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <SearchIcon size={20} color={"#00CCBB"}/>
+          <TextInput 
+          placeholder="Find Restaurants..."
+          keyboardType="default"
+          />
+        </View>
+        <AdjustmentsIcon color={"#00CCBB"}/>
+      </View>
+
+      {/* Body */}
+      <ScrollView
+      className="bg-gray-100"
+      contentContainerStyle={{
+        paddingBottom:100
+      }}
+      >
+        {/* Catagories */}
+        <Categories/>
+        {/* Featured Rows */}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
