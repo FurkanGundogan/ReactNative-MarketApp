b709d8ab49118782700fd831a11e89e17ee3edbf
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-07-23 21:13:38 +0300

    Preparing Order Screen

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import RestaurantScreen from "./screens/RestaurantScreen";
 import { Provider } from 'react-redux'
 import { store } from "./store";
 import BasketScreen from "./screens/BasketScreen";
+import PreparingOrderScreen from "./screens/PreparingOrderScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +23,18 @@ export default function App() {
           <Stack.Screen name="Basket" component={BasketScreen} 
           options={{
             presentation:'modal',
+            headerShown:false
+          }}
+          />
+          <Stack.Screen name="PreparingOrderScreen" component={PreparingOrderScreen} 
+          options={{
+            presentation:'fullScreenModal',
+            headerShown:false
+          }}
+          />
+          <Stack.Screen name="PreparingOrderScreen" component={PreparingOrderScreen} 
+          options={{
+            presentation:'fullScreenModal',
             headerShown:false
           }}
           />
